8f7ff1ab5d164b002ed144be62724bf2eac75590
Bella Hwang <bellahwang15@gmail.com>
2022-12-28 21:25:31 -0500

    cunliffe + networks

---
+++ CunliffeViewer/cunliffe.py
@@ -0,0 +1,142 @@
+from iliadodyssey import returnWorks
+import xml.etree.ElementTree as ET
+
+ns = {"TEI": "http://www.tei-c.org/ns/1.0"}
+
+def lexentries(inputWord, inputCite, searchBy):
+    
+    FILENAME = "cunliffe.lexentries.unicode.xml"
+    tree = ET.parse(FILENAME)
+    root = tree.getroot()
+
+    for entry in root.findall(".//TEI:div[@type='textpart']", ns):
+        lemma = entry.get('n')
+        if (lemma != None):
+            if (lemma  == inputWord):
+                for p in entry.findall(".//TEI:p", ns):
+                    for gloss in p.findall(".//TEI:gloss", ns):
+                        glosstxt = gloss.text
+                        print('Gloss: ' + glosstxt)
+                    for bibl in p.findall(".//TEI:bibl", ns):
+                        biblcite = bibl.get('n')
+                        if (searchBy == 'work'):
+                            if (inputCite == biblcite[0:8]):
+                                print(biblcite)
+                                homerwork = biblcite[5:7]
+                                returnWorks(inputWord, biblcite, homerwork)
+                            elif (inputCite == 'N/A'):
+                                print(biblcite)
+                                homerwork = biblcite[5:7]
+                                returnWorks(inputWord, biblcite, homerwork)
+                        elif (searchBy == 'book'):
+                            if (inputCite[0:11] == biblcite[0:11]):
+                                print(biblcite)
+                                homerwork = biblcite[5:7]
+                                returnWorks(inputWord, biblcite, homerwork)
+                            elif (inputCite == 'N/A'):
+                                print(biblcite)
+                                homerwork = biblcite[5:7]
+                                returnWorks(inputWord, biblcite, homerwork)
+                        elif (searchBy == 'line'):
+                            if (inputCite == biblcite):
+                                print(biblcite)
+                                homerwork = biblcite[5:7]
+                                returnWorks(inputWord, inputCite, homerwork)
+                            elif (inputCite == 'N/A'):
+                                print(biblcite)
+                                homerwork = biblcite[5:7]
+                                returnWorks(inputWord, biblcite, homerwork)
+                        
+def hompers(inputWord, inputCite, searchBy):
+    
+    FILENAME = "cunliffe.hompers.unicode.xml"
+    tree = ET.parse(FILENAME)
+    root = tree.getroot()
+
+    for entry in root.findall(".//TEI:div[@type='textpart']", ns):
+        lemma = entry.get('n')
+        if (lemma != None):
+            if (lemma  == inputWord):
+                for p in entry.findall(".//TEI:p", ns):
+                    for gloss in p.findall(".//TEI:gloss", ns):
+                        glosstxt = gloss.text
+                        print('Gloss: ' + glosstxt)
+                    for note in p.findall(".//TEI:note", ns):
+                        notetxt = note.text
+                        print('Note: ' + notetxt)
+                    for bibl in p.findall(".//TEI:bibl", ns):
+                        biblcite = bibl.get('n')
+                        if (searchBy == 'work'):
+                            if (inputCite == biblcite[0:8]):
+                                print(biblcite)
+                                homerwork = biblcite[5:7]
+                                returnWorks(inputWord, biblcite, homerwork)
+                            elif (inputCite == 'N/A'):
+                                print(biblcite)
+                                homerwork = biblcite[5:7]
+                                returnWorks(inputWord, biblcite, homerwork)
+                        elif (searchBy == 'book'):
+                            if (inputCite[0:11] == biblcite[0:11]):
+                                print(biblcite)
+                                homerwork = biblcite[5:7]
+                                returnWorks(inputWord, biblcite, homerwork)
+                            elif (inputCite == 'N/A'):
+                                print(biblcite)
+                                homerwork = biblcite[5:7]
+                                returnWorks(inputWord, biblcite, homerwork)
+                        elif (searchBy == 'line'):
+                            if (inputCite == biblcite):
+                                print(biblcite)
+                                homerwork = biblcite[5:7]
+                                returnWorks(inputWord, inputCite, homerwork)
+                            elif (inputCite == 'N/A'):
+                                print(biblcite)
+                                homerwork = biblcite[5:7]
+                                returnWorks(inputWord, biblcite, homerwork)
+
+print("This script is a tool to query for lemmas and their citations in the Cunliffe Lexicon.")
+print("Results will be outputted as 'gloss' '-> citations underneath gloss'.")
+print("If no citation is inputted, all known citations will be outputted.")
+print('\n')
+
+print("Enter lemma.")
+inputWord = input("Input: ")
+
+print("Would you like to search by work (Hom. Il., Hom. Od.),")
+print("by book (Hom. Il. 1, Hom Od. 1), or line number? (Hom. Il. 1.1, Hom. Od. 1.1)")
+print("Enter 'work', 'book', or 'line'.")
+searchBy = input("Input: ")
+
+if (searchBy == 'work'):
+    print("Enter citation in the format of 'Hom. Il.' or 'Hom. Od.'. If you would not like to input a citation, input 'N/A'.")
+    inputCite = input("Input: ")
+    print("")
+
+    print("LexEntries results: " + '\n')
+    lexentries(inputWord, inputCite, searchBy)
+    
+    print("HomPers results: " + '\n')
+    hompers(inputWord, inputCite, searchBy)
+elif (searchBy == 'book'):
+    print("Enter citation in the format of 'Hom. Il. 1.', 'Hom. Od. 1.', etc. If you would not like to input a citation, input 'N/A'.")
+    inputCite = input("Input: ")
+    print("")
+
+    print("LexEntries results: " + '\n')
+    lexentries(inputWord, inputCite, searchBy)
+    
+    print("HomPers results: " + '\n')
+    hompers(inputWord, inputCite, searchBy)
+elif (searchBy == 'line'):
+    print("Enter citation in the format of 'Hom. Il. 4.233', 'Hom. Od. 3.22', etc. If you would not like to input a citation, input 'N/A'.")
+    inputCite = input("Input: ")
+    print("")
+
+    print("LexEntries results: " + '\n')
+    lexentries(inputWord, inputCite, searchBy)
+    
+    print("HomPers results: " + '\n')
+    hompers(inputWord, inputCite, searchBy)\
+
+else:
+    print("Invalid. Closing Program.")
---
+++ CunliffeViewer/iliadodyssey.py
@@ -0,0 +1,60 @@
+import englishalignment
+import sys
+import xml.etree.ElementTree as ET
+
+def printGrkSent(inputWord, inputCite, homerWork):
+
+    if (homerWork == "Il"):
+        FILENAME = "tlg0012.tlg001.perseus-grc1.tb.xml"
+    elif (homerWork == "Od"):
+        FILENAME ="tlg0012.tlg002.perseus-grc1.tb.xml"
+        
+    tree = ET.parse(FILENAME)
+    root = tree.getroot()
+    
+    for sentence in root.findall('.//sentence'):
+        sentid = sentence.get('id')
+        for word in sentence.findall('./word'):
+            lemma = word.get('lemma')
+            cite = word.get('cite')
+            if (cite != None and homerWork == "Il"):
+                strippedCite = "Hom. Il. " + cite[32:]
+                if (inputWord == lemma and inputCite == strippedCite):
+                    return sentid
+            elif (cite != None and homerWork == "Od"):
+                strippedCite = "Hom. Od. " + cite[32:]
+                if (inputWord == lemma and inputCite == strippedCite):
+                    return sentid
+
+def returnWorks(inputWord, inputCite, homerWork):
+    
+    if (homerWork == "Il"):
+        FILENAME = "tlg0012.tlg001.perseus-grc1.tb.xml"
+    elif (homerWork == "Od"):
+        FILENAME ="tlg0012.tlg002.perseus-grc1.tb.xml"
+        
+    tree = ET.parse(FILENAME)
+    root = tree.getroot()
+    
+    targetid = printGrkSent(inputWord, inputCite, homerWork)
+    for sentence in root.findall('.//sentence'):
+        sentid = sentence.get('id')
+        if (sentid == targetid):
+            for word in sentence.findall('./word'):
+                form = word.get('form')
+                lemma = word.get('lemma')
+                if (inputWord == lemma):
+                                     #  bold        green               end
+                    sys.stdout.write('\033[1m' + '\33[32m' + form + '\033[0m')
+                    sys.stdout.write(" ")
+                else:
+                    sys.stdout.write(form)
+                    sys.stdout.write(" ")
+    print("")
+    if (homerWork == "Il"):
+        englishalignment.printIlEngSent(targetid)
+        print("")
+    elif (homerWork == "Od"):
+        englishalignment.printOdEngSent(targetid)
+        print("")
+    
